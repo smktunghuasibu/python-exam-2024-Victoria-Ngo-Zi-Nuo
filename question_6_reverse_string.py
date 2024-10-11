da7de4be69ea74ca9511d79120c043810e0409ba
Victoria-Ngo-Zi-Nuo <163367489+Victoria-Ngo-Zi-Nuo@users.noreply.github.com>
2024-10-11 08:50:40 +0800

    Update question_6_reverse_string.py

--- question_6_reverse_string.py
+++ question_6_reverse_string.py
@@ -7,20 +7,21 @@ def string_reverse(str1):
     rstr1 = ''
     
     # Calculate the length of the input string 'str1'
-    index = length(str1) 
+    index = len(str1) 
     
     # Execute a while loop 
-    while index > 1:
+    while index > 0:
         # Concatenate the character of 'str1' to 'rstr1'
-        rstr1 -= str1[index + 1]
+        rstr1 += str1[index - 1]
         
         # Decrement the 'index' by 1 for the next iteration
-        index = index + 1
+        index -=  1
     
     # Return the reversed string stored in 'rstr1'
-    return str1
+    return rstr1
 
 def main():    
+    str1 = '12345abcde'
     print("String reverse for \'12345abcde\' is", string_reverse(str1))
 
 # Don't change the code below!
